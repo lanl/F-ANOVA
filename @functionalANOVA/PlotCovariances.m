8059cc63a1e666ade7dc85a933c29fdb3e61b8b2
Adam Watts <acwatts@lanl.gov>
2025-04-08 16:23:04 -0600

    improved plotting using tiledlayout vs gridplot

--- @functionalANOVA/PlotCovariances.m
+++ @functionalANOVA/PlotCovariances.m
@@ -60,7 +60,6 @@ function PlotCovariances(self, varargin)
 % See also FUNCTIONALANOVA
 
 p = inputParser;
-
 addParameter(p, 'plotType', 'default', @(x) any(strcmpi(x, {'default', 'primary', 'secondary', 'interaction'})))
 addParameter(p, 'SubgroupIndicator', self.SubgroupIndicator, @(x) isnumeric(x) || iscell(x)) % Indicator Array Denoting Subgroups
 addParameter(p, 'GroupLabels', self.GroupLabels, @(x) ischar(x) || isstring(x));
@@ -73,12 +72,10 @@ addParameter(p, 'domainUnitsLabel', self.domainUnitsLabel, @(x) ischar(x) || iss
 addParameter(p, 'responseUnitsLabel', self.responseUnitsLabel, @(x) ischar(x) || isstring(x));
 addParameter(p, 'titleLabels', {}, @(x) iscell(x));
 addParameter(p, 'savePath', '', @(x) ischar(x) || isstring(x));
-addParameter(p, 'position', [90,257,2000,800], @(x) isnumeric(x) || numel(x)==4)
-
+addParameter(p, 'position', [90,257,2000,800], @(x) isnumeric(x) && numel(x)==4)
 
 parse(p, varargin{:})
 self.checkForNewLabels(p)
-
 
 plotType = upper(p.Results.plotType);
 self.SubgroupIndicator = p.Results.SubgroupIndicator;
@@ -130,11 +127,10 @@ else
     end
 end
 
-if ~isempty(self.EchoEnsembleRecs)  % If Echo Ensemble Records (get MetaData)
-
+if ~isempty(self.EchoEnsembleRecs)
     if ~isempty(titleLabels)
-        Title_Labels=self.EchoEnsembleRecs.makeSummaryString(titleLabels, true);
-        saveLabels =self.EchoEnsembleRecs.makeSummaryString(titleLabels, true, 'sanitizeString', true);
+        Title_Labels = self.EchoEnsembleRecs.makeSummaryString(titleLabels, true);
+        saveLabels = self.EchoEnsembleRecs.makeSummaryString(titleLabels, true, 'sanitizeString', true);
     else
         Title_Labels = '';
         saveLabels = '';
@@ -165,156 +161,134 @@ switch plotType
         for K = 1:self.k_groups
             yy = [yy; self.data{K}'];
         end
-
-        bflag  = self.SubgroupIndicator;
-        bflag0=unique(self.SubgroupIndicator); %% levels of Factor B
+        bflag = self.SubgroupIndicator;
+        bflag0 = unique(self.SubgroupIndicator); %% levels of Factor B
         N_Secondary = numel(bflag0);
-
         subData = cell(1, N_Secondary);
-
         for K = 1:N_Secondary
-            flag = bflag==bflag0(K);
+            flag = bflag == bflag0(K);
             subData{K} = yy(flag, :)';
         end
-
         [gamma_hat_i, pooled_covar, n_ii] = MakeCovariances(subData, N_Secondary, self.n_domain_points);
         K = N_Secondary;
 
     case 'INTERACTION'
-
         yy = [];
         for K = 1:self.k_groups
             yy = [yy; self.data{K}'];
         end
-
         aflag  = functionalANOVA.aflagMaker(self.n_i');
         bflag  = self.SubgroupIndicator;
         aflag0 = unique(aflag); %% Levels of Factor A
         p = length(aflag0); %% Number of Factor A's levels
-
-        bflag0=unique(self.SubgroupIndicator); %% levels of Factor B
-        q=length(bflag0); %% Number of Factor B's level
-
-        ab=p*q;  % also known as n in the textbook
+        bflag0 = unique(self.SubgroupIndicator); %% levels of Factor B
+        q = length(bflag0); %% Number of Factor B's levels
+        ab = p*q;  % also known as n in the textbook
         self.AB_groups = ab;
-
         subData = cell(1, ab);
         counter = 1;
-        for i=1:p
-            for j=1:q
-                ijflag=(aflag==aflag0(i))&(bflag==bflag0(j));
-                yyi=yy(ijflag,:);
+        for i = 1:p
+            for j = 1:q
+                ijflag = (aflag == aflag0(i)) & (bflag == bflag0(j));
+                yyi = yy(ijflag,:);
                 subData{counter} = yyi';
                 counter = counter + 1;
             end
         end
-
         [gamma_hat_i, pooled_covar, n_ii] = MakeCovariances(subData, ab, self.n_domain_points);
         K = ab;
-
 end
 
-% Visualize Kernel over entire Domain
-FIG1 = figure('Name', sprintf('%s Covariances Visualized', fig_label), 'units', 'points', 'position', position);
+% Create figure and tiledlayout:
+FIG1 = figure('Name', sprintf('%s Covariances Visualized', fig_label), 'Units', 'points', 'Position', position);
+nPlots = K + 1;  % K group covariances + 1 pooled covariance
+[rows, cols] = GridGenerator(nPlots);
+t = tiledlayout(rows, cols, 'TileSpacing', 'compact', 'Padding', 'compact');
 
-
-[M, N] = GridGenerator(K);
-
-M_exta = M + 1;
-plot_counter = 1;
+% Loop over each group covariance image:
 for ii = 1:K
+    ax = nexttile;
     cmin = min(gamma_hat_i(:, :, ii), [], 'all');
     cmax = max(gamma_hat_i(:, :, ii), [], 'all');
-
-    if mod(plot_counter, M_exta) == 0
-        plot_counter = plot_counter + 1;
-        subh = subplot(N, M_exta, plot_counter);
-    else
-        subh = subplot(N, M_exta, plot_counter);
-    end
-
-    image(self.d_grid, self.d_grid, gamma_hat_i(:,:,ii), "CDataMapping","scaled")
-    AX = gca;
-    AX.YDir = 'normal'; % Corrrects Axis Direction
-    clim(subh,[cmin,cmax])
-    title(display_label(ii) + " | n = " + n_ii(ii))%, "interpreter", 'latex', 'fontSize', 28)
-    C = colorbar;
-    if ~isempty(self.EchoEnsembleRecs)  % If Echo Ensemble Records (use MetaData)
+    
+    image(self.d_grid, self.d_grid, gamma_hat_i(:,:,ii), "CDataMapping", "scaled")
+    daspect([1 1 1]);
+    ax.YDir = 'normal'; % Correct axis direction
+    clim(ax, [cmin, cmax])
+    title(display_label(ii) + " | n = " + n_ii(ii))
+    colorbar(ax);
+    
+    % Copy the y-axis ticks to the x-axis:
+    set(ax, 'XTick', get(ax, 'YTick'));
+    
+    if ~isempty(self.EchoEnsembleRecs)
         if isempty(self.domainUnitsLabel)
             tempLabel = self.domainLabel;
         else
             tempLabel = self.domainLabel + sprintf(' (%s)', self.domainUnitsLabel);
         end
-
     else
         if ~isempty(self.domainUnitsLabel)
             tempLabel = sprintf('(%s)', self.domainUnitsLabel);
         end
     end
-
-    xlabel(tempLabel)
-    ylabel(tempLabel)
-
+    xlabel(ax, tempLabel)
+    ylabel(ax, tempLabel)
+    
     if isempty(xScale)
-        set(gca,'Xscale','linear')
+        set(ax, 'XScale', 'linear')
     else
-        set(gca,'Xscale', xScale)
+        set(ax, 'XScale', xScale)
     end
-
+    
     if isempty(yScale)
-        set(gca,'Yscale', 'linear')
+        set(ax, 'YScale', 'linear')
     else
-        set(gca,'Yscale', yScale)
+        set(ax, 'YScale', yScale)
     end
-
-    plot_counter = plot_counter + 1;
 end
 
-% Pooled Options
-
+% Pooled Covariance Tile:
+ax = nexttile;
 cmin = min(pooled_covar, [], 'all');
 cmax = max(pooled_covar, [], 'all');
-
-subh = subplot(1, K + 1, K + 1);
-image(self.d_grid, self.d_grid, pooled_covar, "CDataMapping","scaled")
-AX = gca;
-AX.YDir = 'normal';  % Corrrects Axis Direction
-clim(subh,[cmin,cmax])
+image(self.d_grid, self.d_grid, pooled_covar, "CDataMapping", "scaled")
+daspect([1 1 1]);
+ax.YDir = 'normal';
+clim(ax, [cmin, cmax])
 title("Pooled" + " | n = " + self.N)
-xlabel(tempLabel)
-ylabel(tempLabel)
-sgtitle({sprintf('%s Covariances', fig_label),Title_Labels})
+xlabel(ax, tempLabel)
+ylabel(ax, tempLabel)
+colorbar(ax);
+set(ax, 'XTick', get(ax, 'YTick'));  % Copy y ticks to x ticks
 
-C = colorbar;
+% Add an overall title for the tiled layout:
+set(findall(FIG1, '-property', 'FontSize'), 'FontSize', 18)
+sgtitle({sprintf('%s Covariances', fig_label), Title_Labels},  FontSize=20)
 
-title(C, color_barLabel)
-set(findall(gcf, '-property', 'FontSize'), 'FontSize', 18)
 
 saveLabels = "Covariance_" + saveLabels;
-
-
 if isfolder(savePath)
     saveas(FIG1, fullfile(savePath, saveLabels), 'png')
     close(FIG1)
 end
 
-
 if isempty(xScale)
-    set(gca,'Xscale','linear')
+    set(gca, 'XScale', 'linear')
 else
-    set(gca,'Xscale', xScale)
+    set(gca, 'XScale', xScale)
 end
 
 if isempty(yScale)
-    set(gca,'Yscale', 'linear')
+    set(gca, 'YScale', 'linear')
 else
-    set(gca,'Yscale', yScale)
+    set(gca, 'YScale', yScale)
 end
 
 if isempty(colorScale)
-    set(gca,'ColorScale', 'linear')
+    set(gca, 'ColorScale', 'linear')
 else
-    set(gca,'ColorScale', colorScale)
+    set(gca, 'ColorScale', colorScale)
 end
 
 end
@@ -322,66 +296,49 @@ end
 function [gamma_hat_i, pooled_covar, n_ii] = MakeCovariances(DATA, K, domainPoints)
 % Compute basic group means and covariances
 eta_i = zeros(domainPoints, K); % estimated means
-v_hat_i = cell(K); %residuals matrix
-gamma_hat_i = zeros(domainPoints, domainPoints,K); % groupwise covariance
+v_hat_i = cell(K); % residuals matrix
+gamma_hat_i = zeros(domainPoints, domainPoints, K); % group-wise covariance
 n_ii = zeros(1, K);
 for kk = 1:K
     n_i = size(DATA{kk}, 2);
     n_ii(kk) = n_i;
-    eta_i(:, kk) = mean(DATA{kk}, 2); %estimated means
-    zeroMeanData_k_subset = DATA{kk} - eta_i(:, kk); %residuals
-    v_hat_i{kk} = zeroMeanData_k_subset; %residuals
-    gamma_hat_i(:,:,kk) = 1 / (n_i - 1) .* (zeroMeanData_k_subset * zeroMeanData_k_subset'); % groupwise covariance
+    eta_i(:, kk) = mean(DATA{kk}, 2); % estimated means
+    zeroMeanData_k_subset = DATA{kk} - eta_i(:, kk); % residuals
+    v_hat_i{kk} = zeroMeanData_k_subset;
+    gamma_hat_i(:,:,kk) = 1 / (n_i - 1) .* (zeroMeanData_k_subset * zeroMeanData_k_subset');
 end
 
-
 N = sum(n_ii);
-% Compute pooled covariance
 pooled_covar_terms = zeros(domainPoints, domainPoints, K);
 for kk = 1:K
     pooled_covar_terms(:,:,kk) = (n_ii(kk) - 1) .* gamma_hat_i(:,:,kk);
 end
-
 pooled_covar = sum(pooled_covar_terms, 3) ./ (N - K);
 
 end
 
 function [M, N] = GridGenerator(K)
-
-%Check if Odd or Even
-if mod(K, 2) == 0
-    EvenGroups = true;
-else
-    EvenGroups = false;
-end
-
+% Compute grid dimensions for K plots.
 if K <= 3
-    N = 1;
-    M = K;
+    N = K;
+    M = 1;
 elseif K == 4
     N = 2;
     M = 2;
 elseif K == 5
-    N = 3;
-    M = 2;
-
-elseif K == 6
-    N = 3;
-    M = 2;
-elseif K == 7
-    N = 4;
+    N = 2;
     M = 3;
-elseif K >= 8 && EvenGroups
-    N = 4;
+elseif K == 6
+    N = 2;
+    M = 3;
+elseif K == 7
+    N = 3;
+    M = 3;
+elseif K == 8
+    N = 3;
+    M = 3;
+else
+    N = ceil(sqrt(K));
     M = ceil(K/N);
-elseif K >= 9 && ~EvenGroups
-    if floor(sqrt(K))^2 == K
-        N = sqrt(K);
-        M = N;
-    else
-        N = 4;
-        M = ceil(K/N);
-    end
 end
-
 end
